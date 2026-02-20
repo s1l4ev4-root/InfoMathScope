833afadf08759070ffced1f49d176cd6508e5d47
s1l4ev4-root <silaevaalina@gmail.com>
2026-02-20 20:50:37 +0300

    add preloader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from "react";
 import styles from "./App.module.scss";
-import HomePage from "@/pages/HomePage";
+import HomePage from "../pages/HomePage";
+import { Preloader } from "../shared/ui/Preloader";
+import classNames from "classnames";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [hiding, setHiding] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          setTimeout(() => setHiding(true), 300);
+          setTimeout(() => setLoading(false), 1100);
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 20);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className={styles.scrollContainer}>
+        <HomePage />
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.scrollContainer}>
-      <HomePage />
+    <div
+      className={classNames(styles.preloaderContainer, {
+        [styles.hiding]: hiding,
+      })}
+    >
+      <Preloader progress={progress} />
     </div>
   );
 }
