f049851e679cceccb83f5d8a0ad0ab2abfc62c76
s1l4ev4-root <silaevaalina@gmail.com>
2026-02-20 16:46:51 +0300

    add github pages to project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/InfoMathScope/",
   server: {
     host: true,
     port: 5173,
